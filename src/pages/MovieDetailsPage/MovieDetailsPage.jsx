8b86ec8806b552c2cfd74628c40338340f20452e
Andrey Shemet <an-shem@ukr.net>
2021-06-15 00:11:16 +0300

    почистил комментарии

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-// import PropTypes from 'prop-types';
 import { Link, Route, Switch } from 'react-router-dom';
 import Cast from '../../components/Cast';
 import Reviews from '../../components/Reviews';
